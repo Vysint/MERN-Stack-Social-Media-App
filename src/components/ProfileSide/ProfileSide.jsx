b9dc1fab6251a07b718131ddee83190a32a2b91a
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-18 23:20:16 +0300

    Added Profile Card component Markup and it's css styling

--- src/components/ProfileSide/ProfileSide.jsx
+++ src/components/ProfileSide/ProfileSide.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LogoSearch from '../LogoSearch/LogoSearch';
+import ProfileCard from "../ProfileCard/ProfileCard";
 import "./ProfileSide.css";
 
 const ProfileSide = () => {
   return (
     <div className="profileSide">
       <LogoSearch/>
+      <ProfileCard/>
     </div>
   )
 };
