ff1aec86a36fcf38f04af1931e51eac18dc8eec3
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-06 23:53:30 +0300

    Added jwt token validation

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 // Registering a new User
@@ -16,8 +16,16 @@ exports.signup = async (req, res, next) => {
     if (existingUser) {
       return res.status(400).json({ message: "Email already exists!" });
     }
-    await newUser.save();
-    res.status(200).json(newUser);
+    const user = await newUser.save();
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user._id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ user, token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -37,10 +45,17 @@ exports.login = async (req, res) => {
         password,
         identifiedUser.password
       );
+      if (!validPassword) {
+        res.status(400).json("You entered a wrong password.");
+      } else {
+        const token = jwt.sign(
+          { email: identifiedUser.email, id: identifiedUser._id },
+          process.env.JWT_KEY,
+          { expiresIn: "1h" }
+        );
 
-      validPassword
-        ? res.status(200).json(identifiedUser)
-        : res.status(400).json("You entered the wrong Password");
+        res.status(200).json({ identifiedUser, token });
+      }
     } else {
       res.status(404).json("User does not exist");
     }
