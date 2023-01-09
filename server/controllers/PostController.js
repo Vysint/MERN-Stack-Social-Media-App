df7598ce46baa865117a274fdd860ef50a9f3a79
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-09 16:57:12 +0300

    Added minor changes

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -9,7 +9,7 @@ exports.createPost = async (req, res) => {
 
   try {
     await newPost.save();
-    res.status(200).json("Post created!");
+    res.status(200).json(newPost);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
