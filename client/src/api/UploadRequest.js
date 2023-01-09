bc04f10407a408caed65043bb27fe9c274042901
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-09 16:54:34 +0300

    Added API Call for POSTS

--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5001" });
 
 export const uploadImage = (data) => API.post("/upload/", data);
+
+export const uploadPost = (data) => API.post("/posts", data);
