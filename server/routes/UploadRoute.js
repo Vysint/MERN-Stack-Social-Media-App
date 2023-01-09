a6ebc6d9039eb6fdae7b01427dc1b2d2d9bbb004
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-09 16:58:17 +0300

    Added an api endpoint for uploading images

---
+++ server/routes/UploadRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const multer = require("multer");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post(
+  "/",
+  upload.single("file", (req, res) => {
+    try {
+      return res.status(200).json("File uploaded successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  })
+);
+module.exports = router;
