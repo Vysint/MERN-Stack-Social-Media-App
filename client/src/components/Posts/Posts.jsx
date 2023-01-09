316155902669fec730444b3e39c438e6c63a4e54
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-09 18:16:34 +0300

    Added Posts data fetching from the backend || server

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import './Posts.css'
-import { PostsData } from '../../Data/PostsData'
-import Post from '../Post/Post'
+import React, { useEffect } from "react";
+import Post from "../Post/Post";
+import { useDispatch, useSelector } from "react-redux";
+
+import "./Posts.css";
+import { getTimelinePosts } from "../../actions/postAction";
 const Posts = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const { posts, loading } = useSelector((state) => state.postReducer);
+
+  useEffect(() => {
+    dispatch(getTimelinePosts(user._id));
+  }, [dispatch, user._id]);
   return (
     <div className="Posts">
-        {PostsData.map((post, id)=>{
-            return <Post data={post} id={id}/>
-        })}
+      {loading
+        ? "Fetching Posts...."
+        : posts.map((post, id) => {
+            return <Post data={post} id={id} />;
+          })}
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
