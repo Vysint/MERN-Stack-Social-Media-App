fae7951ac7c40786d1642c6e951e54de1f8b279b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-09 18:15:25 +0300

    Added Posts data fetching from the backend || server

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import './Post.css'
-import Comment from '../../img/comment.png'
-import Share from '../../img/share.png'
-import Heart from '../../img/like.png'
-import NotLike from '../../img/notlike.png'
+import React from "react";
+import { useSelector } from "react-redux";
+import "./Post.css";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
 
-
-const Post = ({data}) => {
+const Post = ({ data }) => {
+  // const { user } = useSelector((state) => state.authReducer.authdata);
   return (
     <div className="Post">
-        <img src={data.img} alt="" />
+      <img
+        src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""}
+        alt=""
+      />
 
+      <div className="postReact">
+        <img src={data.liked ? Heart : NotLike} alt="" />
+        <img src={Comment} alt="" />
+        <img src={Share} alt="" />
+      </div>
 
-        <div className="postReact">
-            <img src={data.liked?Heart: NotLike} alt="" />
-            <img src={Comment} alt="" />
-            <img src={Share} alt="" />
-        </div>
+      <span style={{ color: "var(--gray)", fontSize: "12px" }}>
+        {data.likes} likes
+      </span>
 
-
-        <span style={{color: "var(--gray)", fontSize: '12px'}}>{data.likes} likes</span>
-
-        <div className="detail">
-            <span><b>{data.name}</b></span>
-            <span> {data.desc}</span>
-        </div>
+      <div className="detail">
+        <span>
+          <b>{data.name}</b>
+        </span>
+        <span> {data.desc}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
