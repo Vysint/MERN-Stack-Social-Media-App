ba25c46cd937cf458b42290644e1163f19175e4f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-10 13:23:23 +0300

    Likes API Call

--- client/src/api/PostRequest.js
+++ client/src/api/PostRequest.js
@@ -3,3 +3,6 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5001" });
 
 export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`);
+
+export const likePost = (id, userId) =>
+  API.put(`/posts/${id}/likes`, { userId: userId });
