c3a657ec65924d3f769f616fea62390bb1e6fc62
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-10 13:24:40 +0300

    Added likes function handler

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,11 +5,19 @@ import Comment from "../../img/comment.png";
 import Share from "../../img/share.png";
 import Heart from "../../img/like.png";
 import NotLike from "../../img/notlike.png";
+import { likePost } from "../../api/PostRequest";
 
 const Post = ({ data }) => {
   const { user } = useSelector((state) => state.authReducer.authData);
 
   const [liked, setLiked] = useState(data.likes.includes(user._id));
+  const [likes, setLikes] = useState(data.likes.length);
+
+  const handleLike = () => {
+    setLiked((prev) => !prev);
+    likePost(data._id, user._id);
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+  };
   return (
     <div className="Post">
       <img
@@ -18,13 +26,18 @@ const Post = ({ data }) => {
       />
 
       <div className="postReact">
-        <img src={data.liked ? Heart : NotLike} alt="" />
+        <img
+          src={liked ? Heart : NotLike}
+          alt=""
+          style={{ cursor: "pointer" }}
+          onClick={handleLike}
+        />
         <img src={Comment} alt="" />
         <img src={Share} alt="" />
       </div>
 
       <span style={{ color: "var(--gray)", fontSize: "12px" }}>
-        {data.likes} likes
+        {likes} likes
       </span>
 
       <div className="detail">
