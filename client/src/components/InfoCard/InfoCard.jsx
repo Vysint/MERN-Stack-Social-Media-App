69f5404116e2c6b393860e5d1e4c52a80c5f91aa
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-10 16:34:29 +0300

    Retrieved and displayed user information in the profile

--- client/src/components/InfoCard/InfoCard.jsx
+++ client/src/components/InfoCard/InfoCard.jsx
@@ -1,49 +1,83 @@
-import React, { useState } from "react";
-import "./InfoCard.css";
+import React, { useState, useEffect } from "react";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from "../ProfileModal/ProfileModal";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import * as UserApi from "../../api/UseRequest";
+import {logOut} from '../../actions/AuthAction'
+import "./InfoCard.css";
 
 const InfoCard = () => {
   const [modalOpened, setModalOpened] = useState(false);
+
+  const dispatch = useDispatch();
+  const params = useParams();
+
+  const profileUserId = params.id;
+  const [profileUser, setProfileUser] = useState({});
+
+  const { user } = useSelector((state) => state.authReducer.authData);
+
+  useEffect(() => {
+    const fetchProfileUser = async () => {
+      if (profileUserId === user._id) {
+        setProfileUser(user);
+      } else {
+        const profileUser = await UserApi.getUser(profileUserId);
+        setProfileUser(profileUser);
+      }
+    };
+    fetchProfileUser();
+  }, [profileUserId, user]);
+
+  const handleLogout = () => {
+    dispatch(logOut())
+  }
   return (
     <div className="InfoCard">
       <div className="infoHead">
-        <h4>Your Info</h4>
-        <div>
-          <UilPen
-            width="2rem"
-            height="1.2rem"
-            onClick={() => setModalOpened(true)}
-          />
-          <ProfileModal
-            modalOpened={modalOpened}
-            setModalOpened={setModalOpened}
-          />
-        </div>
+        <h4>Profile Info</h4>
+        {user._id === profileUserId ? (
+          <div>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+            <ProfileModal
+              modalOpened={modalOpened}
+              setModalOpened={setModalOpened}
+            />
+          </div>
+        ) : (
+          ""
+        )}
       </div>
 
       <div className="info">
         <span>
           <b>Status </b>
         </span>
-        <span>in Relationship</span>
+        <span>{profileUser.relationship}</span>
       </div>
 
       <div className="info">
         <span>
           <b>Lives in </b>
         </span>
-        <span>Multan</span>
+        <span>{profileUser.livesin}</span>
       </div>
 
       <div className="info">
         <span>
           <b>Works at </b>
         </span>
-        <span>Zainkeepscode inst</span>
+        <span>{profileUser.worksAt}</span>
       </div>
 
-      <button className="button logout-button">Logout</button>
+      <button className="button logout-button" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
