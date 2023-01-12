083144a655648b3e2b43019ab8e2d6f7f1eb1df9
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-12 22:11:09 +0300

    Altered the dynamically rendered information

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const User = ({ person }) => {
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="follower">
       <div>
-        <img src={person.img} alt="" className="followerImage" />
+        <img
+          src={
+            user.coverPicture
+              ? serverPublic + user.profilePicture
+              : serverPublic + "defaultProfile.png"
+          }
+          alt=""
+          className="followerImage"
+        />
         <div className="name">
-          <span>{person.name}</span>
-          <span>@{person.username}</span>
+          <span>{person.firstname}</span>
+          <span>@{person.lastname}</span>
         </div>
       </div>
       <button className="button fc-button">Follow</button>
