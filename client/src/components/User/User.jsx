494e273aff07e54a7058c617dbee7a638021c0b6
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-12 12:08:29 +0300

    Added  a User component

---
+++ client/src/components/User/User.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const User = ({ person }) => {
+  return (
+    <div className="follower">
+      <div>
+        <img src={person.img} alt="" className="followerImage" />
+        <div className="name">
+          <span>{person.name}</span>
+          <span>@{person.username}</span>
+        </div>
+      </div>
+      <button className="button fc-button">Follow</button>
+    </div>
+  );
+};
+
+export default User;
