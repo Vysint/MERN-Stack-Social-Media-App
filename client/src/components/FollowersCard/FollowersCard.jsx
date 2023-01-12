a2b05e562b2d35b868897852696d589d80de8048
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-12 22:12:40 +0300

    Dynamically rendered followers

--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import './FollowersCard.css'
-
-import { Followers } from '../../Data/FollowersData'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getAllUsers } from "../../api/UserRequest";
+import User from "../User/User";
+import "./FollowersCard.css";
 const FollowersCard = () => {
+  const [persons, setPersons] = useState([]);
+  const { user } = useSelector((state) => state.authReducer.authData);
+
+  useEffect(() => {
+    const fetchPersons = async () => {
+      const { data } = await getAllUsers();
+      setPersons(data);
+      console.log(data);
+    };
+    fetchPersons();
+  }, []);
+
   return (
     <div className="FollowersCard">
-        <h3>Who is following you</h3>
+      <h3>People you may know</h3>
 
-        {Followers.map((follower, id)=>{
-            return(
-                <div className="follower">
-                    <div>
-                        <img src={follower.img} alt="" className='followerImage' />
-                        <div className="name">
-                            <span>{follower.name}</span>
-                            <span>@{follower.username}</span>
-                        </div>
-                    </div>
-                    <button className='button fc-button'>
-                        Follow
-                    </button>
-                </div>
-            )
-        })}
+      {persons.map((person, id) => {
+        if (person._id !== user._id) return <User person={person} key={id} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default FollowersCard
+export default FollowersCard;
