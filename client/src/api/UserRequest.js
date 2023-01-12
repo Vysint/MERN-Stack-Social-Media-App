04a14bcefc0b0953ccc6e1b2446fed6c9685e2b8
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-12 22:11:43 +0300

    Api call for retrieve all app users

--- client/src/api/UserRequest.js
+++ client/src/api/UserRequest.js
@@ -5,3 +5,5 @@ const API = axios.create({ baseURL: "http://localhost:5001" });
 export const getUser = (userId) => API.get(`/user/${userId}`);
 
 export const updateUser = (id, formData) => API.put(`/user/${id}`, formData);
+
+export const getAllUsers = () => API.get(`/user`)
